e729b0c69da492fa1c39e932fd502b8dabedd0f7
悠人 菅原 <220369@jc-21.jp>
2023-08-30 12:58:25 +0900

    das

--- Stage.cpp
+++ Stage.cpp
@@ -1,19 +1,23 @@
 #include "Stage.h"
 #include "Engine/Model.h"
+#include "Engine/Input.h"
 
 Stage::Stage(GameObject* parent)
     :GameObject(parent, "Stage")
 {
-    for (int i = 0; i < BLOCK_MAX; i++)hModel_[i] = -1;
+    for (int i = 0; i < MAX; i++)hModel_[i] = -1;
 
     for (int x = 0; x < XSIZE; x++){
-        for (int z = 0; z < ZSIZE; z++)table_[x][z] = 0;
+        for (int z = 0; z < ZSIZE; z++) {
+            SetBlock(x, z,DEFAULT);
+            SetBlockHeight(x, z, 1);
+        }
     }
 }
 
 void Stage::Initialize()
 {
-    string modelFileName[BLOCK_MAX] = {
+    string modelFileName[MAX] = {
         "BoxDefault.fbx",
         "BoxBrick.fbx",
         "BoxGrass.fbx",
@@ -22,15 +26,20 @@ void Stage::Initialize()
     };
     string FilePath = "Assets/MapEditor/";
 
-    for (int i = 0; i < BLOCK_MAX; i++) {
+    for (int i = 0; i < MAX; i++) {
         hModel_[i] = Model::Load(FilePath + modelFileName[i]);
         assert(hModel_[i] >= 0);
     }
 
     //table_にブロックのタイプをセットしてやろう
     for (int x = 0; x < XSIZE; x++) {
-        for (int z = 0; z < ZSIZE; z++)SetBlock(x,z,(x+z%5));
+        for (int z = 0; z < ZSIZE; z++) {
+            SetBlock(x, z, (x%MAX));
+            SetBlockHeight(x, z, (x%3+1));
+        }
     }
+
+    
 }
 
 void Stage::Update()
@@ -39,20 +48,18 @@ void Stage::Update()
 
 void Stage::Draw()
 {
-    Transform BlockTrans;//Transform型の変数を作る
-    //Transform型は、位置向き大きさなどを扱う型
+    Transform BlockTrans;
 
-    for (int width = 0; width < 15; width++)
-    {
-        for (int depth = 0; depth < 15; depth++)
-        {
-            BlockTrans.position_.x = width;
-            BlockTrans.position_.z = depth;
-            int type = table_[width][depth];
+    for (int width = 0; width < 15; width++){
+        for (int depth = 0; depth < 15; depth++){
+            for (int height = 0; height < table_[width][depth].height_; height++) {
 
-            Model::SetTransform(hModel_[type], BlockTrans);
-            Model::Draw(hModel_[type]);
+                BlockTrans.position_ = { (float)width,(float)height,(float)depth };
+                int type = table_[width][depth].type_;
 
+                Model::SetTransform(hModel_[type], BlockTrans);
+                Model::Draw(hModel_[type]);
+            }
         }
     }
 }
--- Stage.h
+++ Stage.h
@@ -1,26 +1,27 @@
 #pragma once
 #include "Engine/GameObject.h"
 
-enum BLOCK_TYPE{
-	DEFAULT,
-	BRICK,
-	GRASS,
-	SAND,
-	WATER,
-	BLOCK_MAX,
-};
+
 
 namespace {
 	const int MODEL_NUM{ 5 };
 	const int XSIZE{ 15 };
 	const int ZSIZE{ 15 };
+
+	enum BLOCK_TYPE {
+		DEFAULT, BRICK, GRASS, SAND, WATER, MAX,
+	};
+	
 }
 
 class Stage : public GameObject
 {
 private:
-	int hModel_[BLOCK_MAX];
-	int table_[XSIZE][ZSIZE];
+	int hModel_[MAX];
+	struct BlockData {
+		int type_;
+		int height_;
+	}table_[XSIZE][ZSIZE];
 
 public:
 	Stage(GameObject* parent);
@@ -29,8 +30,15 @@ public:
 	void Draw() override;
 	void Release() override;
 
-	void SetBlock(int x, int z, BLOCK_TYPE _type) { table_[x][z] = _type; }
-	void SetBlock(int x, int z, int _type) { table_[x][z] = _type; }
+	/// <summary>
+	/// 指定した位置にブロックを設定する
+	/// </summary>
+	/// <param name="x">テーブル座標X</param>
+	/// <param name="z">テーブル座標Y</param>
+	/// <param name="_type">ブロックタイプ</param>
+	void SetBlock(int x, int z, int _type) { table_[x][z].type_ = _type; }
+
+	void SetBlockHeight(int x,int z,int _height){ table_[x][z].height_ = _height; }
 
 };
 
